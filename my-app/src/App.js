235e90aa1dfed6ae2a3b54c91b92a68e7581a604
Nicholas Gilpin <nicholascgilpin123@gmail.com>
2017-09-22 14:31:24 -0500

    Added missing react files. Added form to home page.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class ItemForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    // console.log('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -13,7 +44,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, enter the items you need below.
         </p>
+				<div>
+					<ItemForm text="Add your items here" class="form"/>
+				</div>
       </div>
+
     );
   }
 }
